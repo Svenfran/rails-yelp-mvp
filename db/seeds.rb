79cb3ad7e9f966a7ac45c52d7088335b8deb84d3
Sven Haider <sven.haider@web.de>
2020-05-06 19:09:48 +0200

    implemented routing, controller, model and view for restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "belgian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+curry_king =  { name: "Curry King", address: "282 Kevin Brook, Imogeneborough, CA 58517", category: "belgian" }
+madame_hu =  { name: "Madame Hu", address: "Gollierstraße 20, 80339 München", category: "chinese" }
+javi =  { name: "JaVi", address: "Schleißheimer Str. 182, 80797 München", category: "japanese" }
+
+[ dishoom, pizza_east, curry_king, madame_hu, javi ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
